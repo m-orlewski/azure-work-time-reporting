775e199a8e884a9c0c3f0c6f1e1809583d4d4a5e
Michal Orlewski <michal.orlewski@ericsson.com>
2024-01-03 11:36:52 +0100

    allowed host literal

--- azureproject/production.py
+++ azureproject/production.py
@@ -5,7 +5,8 @@ from .settings import BASE_DIR
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+# ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ['django-web-test-mo.azurewebsites.net']
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
 
